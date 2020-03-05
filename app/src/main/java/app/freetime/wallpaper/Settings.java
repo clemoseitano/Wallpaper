a6bdaf1d3bdf28d9316f514ff8f664a6a3080ef9
clemoseitano <clemoseitano@outlook.com>
2020-03-05 18:41:47 +0000

    Updated gradle and android versions, migrated to androidx

--- app/src/main/java/app/freetime/wallpaper/Settings.java
+++ app/src/main/java/app/freetime/wallpaper/Settings.java
@@ -5,9 +5,9 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 
